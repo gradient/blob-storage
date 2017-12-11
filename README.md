6f9c5009ad06816e255f776557d352e70d0e06f8
Elliot Blackburn <Elliot.blackburn@gmail.com>
2017-12-11 12:42:32 +0000

    Prep for development

---
+++ README.md
@@ -0,0 +1,13 @@
+# Blob Storage
+
+> Blob storage at your finger tips
+
+Blob storage can be awkward to orchestrate, especially when you need to switch out providers for your project on demand. Blob storage makes this easier, it comes pre-bundled with a number of pre-configured providers. Blob storage is opinionated and designed to work with minimal fuss and configuration.
+
+## Licence
+
+[MIT](./LICENSE.md) @ [Gradient Limited](https://gradient.co/)
+
+## Code of conduct
+
+Please note that this project is released with a [Contributor Code of Conduct](code-of-conduct.md). By participating in this project you agree to abide by its terms.
--- src/clients/s3.ts
+++ src/clients/s3.ts
@@ -1,8 +1,8 @@
-import {BlobItem, BlobStorageClient, GenericObject} from '../../definitions';
+import { BlobItem, BlobStorageClient, GenericObject } from '../definitions';
 import AWS = require('aws-sdk');
 import S3 = require('aws-sdk/clients/s3');
 import * as config from 'config';
-import { itemTypes} from '../item-types';
+import { itemTypes } from '../item-types';
 import * as Bluebird from 'bluebird';
 
 const buckets: GenericObject = config.get('aws.buckets');
@@ -13,11 +13,15 @@ const s3 = new S3({
   credentials: {
     accessKeyId: config.get('aws.aws_access_key_id'),
     secretAccessKey: config.get('aws.aws_secret_access_key'),
-  }
+  },
 });
 
 export class BlobStorageS3 implements BlobStorageClient {
-  async getItem(key: string, customerId: string, itemType: string): Promise<any> {
+  async getItem(
+    key: string,
+    customerId: string,
+    itemType: string
+  ): Promise<any> {
     return getItemS3(key, customerId, itemType);
   }
 
@@ -25,29 +29,61 @@ export class BlobStorageS3 implements BlobStorageClient {
     return getMultipleItemS3(items, attempts);
   }
 
-  async setItem(key: string, customerId: string, itemType: string): Promise<any> {
+  async setItem(
+    key: string,
+    customerId: string,
+    itemType: string
+  ): Promise<any> {
     return setItemS3(key, customerId, itemType);
   }
 
-  async deleteItem(key: string, customerId: string, itemType: string): Promise<any> {
+  async deleteItem(
+    key: string,
+    customerId: string,
+    itemType: string
+  ): Promise<any> {
     return deleteItemS3(key, customerId, itemType);
   }
 }
 
-async function getItemS3(key: string, customerId: string, itemType: string): Promise<any> {}
+async function getItemS3(
+  key: string,
+  customerId: string,
+  itemType: string
+): Promise<any> {}
 
-async function getMultipleItemS3(items: BlobItem[], attempts?: number): Promise<any> {}
+async function getMultipleItemS3(
+  items: BlobItem[],
+  attempts?: number
+): Promise<any> {}
 
-async function setItemS3(key: string, value: ReadableStream, customerId: string, itemType: string): Promise<any> {
+async function setItemS3(
+  key: string,
+  value: ReadableStream,
+  customerId: string,
+  itemType: string
+): Promise<any> {
   const bucket = buckets[itemType];
-  const s3obj = new S3({params: {
-    bucket,
-    key
-  }});
+  const s3obj = new S3({
+    params: {
+      bucket,
+      key,
+    },
+  });
 
-  return s3obj.upload({Body: value})
-    .on('httpUploadProgress', function(evt) { console.log(evt); })
-    .send(function(err, data) { console.log(err, data) }).promise();
+  return s3obj
+    .upload({ Body: value })
+    .on('httpUploadProgress', function(evt) {
+      console.log(evt);
+    })
+    .send(function(err, data) {
+      console.log(err, data);
+    })
+    .promise();
 }
 
-async function deleteItemS3(key: string, customerId: string, itemType: string): Promise<any> {}
+async function deleteItemS3(
+  key: string,
+  customerId: string,
+  itemType: string
+): Promise<any> {}
